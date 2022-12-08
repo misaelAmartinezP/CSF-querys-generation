4f5a549c7d0e6d37df55cd26b38de43021e87b8e
misaelAmartinezP <mmartinez@alhel.com.mx>
2022-12-08 10:57:57 -0600

    push-08-12-2022

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -2,9 +2,8 @@
 from PyPDF2 import PdfFileReader
 import os
 
-
-def ConviertePDFaTXT(a):
-    with open(a, "rb") as filehandle:
+def ConviertePDFaTXT(archivo, nombreAr):
+    with open(archivo, "rb") as filehandle:
         pdf = PdfFileReader(filehandle)
         print("Leyendo Paginas de la CSF")
         os.system("Pause")
@@ -24,11 +23,11 @@ def ConviertePDFaTXT(a):
         print("Generando archivo .txt")
         print("**************************************")
         print ("Guardando los datos de CSF hoja 1")
-        file1=open(r"C:\Users\mmartinez\Documents\Tapi America Constancia de Situacion Fiscal 14-11-22.txt","w")
+        file1=open(r"archivo","w")    
         file1.writelines(guardaRFC)
         print("**************************************")
         print ("Guardando los datos de CSF hoja 2")
-        file1=open(r"C:\Users\mmartinez\Documents\Tapi America Constancia de Situacion Fiscal 14-11-22.txt","a")
+        file1=open(nombreAr.txt,"ra")
         file1.writelines(guardaRFC1)
         print("**************************************")
         print("Archivo .TXT creado")
@@ -36,26 +35,17 @@ def ConviertePDFaTXT(a):
         os.system("Pause")
         return file1
 
-def depurarInformacion(file1):
-    print("Depurando campos del  CSF que no sean de utilidad")
-    print("**************************************")
-    print("\n *depurando* \n")
-    file1=open(r"C:\Users\mmartinez\Documents\Tapi America Constancia de Situacion Fiscal 14-11-22.txt","w")
-    print("**************************************")
-    print("Tratamiento de cadenas")
-    print("**************************************")
-    print("Validacion de cadenas")
-    print("**************************************")
-    print("Archivo .TXT depurado ")
-    print("**************************************")
 
 def Inicio():
-    print ("Este herramienta convierte de pdf a txt \nRealiza la extraccion de datos de una CSF \nPosteriormente arroja la sentencia SQL para realizar un update/insert")
-    os.system("Pause \n")
+    print ("Este herramienta convierte de pdf a txt \nRealiza la extraccion de datos de una CSF \nPosteriormente arroja la sentencia SQL para realizar un update/insert \n")
+    os.system("Pause")
+    nombreAr = input("introduzca el nombre del archivo que se encuentra en la direccion: C:/Users/mmartinez/Documents/")
+    #aqui se deberia cambiar la ruta dependiendo de la computadora y en donde se tengan las CSF
     print("**************************************")
-    a = "C:/Users/mmartinez/Documents/Tapi America Constancia de Situacion Fiscal 14-11-22.pdf"
-    ConviertePDFaTXT(a)
-    b = "C:/Users/mmartinez/Documents/Tapi America Constancia de Situacion Fiscal 14-11-22.txt" 
-    depurarInformacion(a)
-    
+    #print (archivoIn)
+    archivo =  os.path.join("C:/Users/mmartinez/Documents/" + nombreAr +".pdf")
+    ConviertePDFaTXT(archivo, nombreAr)
+    #print(archivo)
+
+
 Inicio()
