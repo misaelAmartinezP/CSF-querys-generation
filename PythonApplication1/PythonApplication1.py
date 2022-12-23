f5cf8f70f9bb16ecb7659b0c6c62ad3a05dd7f31
misaelAmartinezP <mmartinez@alhel.com.mx>
2022-12-23 12:04:05 -0600

    nombre de vialidad y numero interior resueltos 23/12/2022

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,7 +1,7 @@
 import aspose.words as aw
 import os
 
-def conviertePDFaTXT(archivo, nombreAr):
+def conviertePDFaTXT(archivo, nombreAr):#convertir el archivo dpf a txt 
         doc = aw.Document(archivo)
         print("Generando el .txt")
         doc.save("C:/Users/mmartinez/Documents/CSF/" + nombreAr +".txt")
@@ -14,20 +14,30 @@ def borrar(nombreAr):
         l1 = fp.readlines()
     with open(archivoTxt, 'w') as fp:
         for number, line in enumerate(l1):
-           if number in [4,5,6,45,46,47,48,50,51,52,53,54,67,69]:
+           if number in [4,5,6,45,46,47,48,49,50,51,52,53,54,67,69]:
                if number == 4: #RFC
-                    if (len(line) == 12):
+                    print(number, line)
+                    print(len(line))
+                    if (len(line) == 12):#corroborar longitud del RFC
                          fp.write(line)
+                         print(line)
                     elif(len(line)== 13):
                          fp.write(line)
+                         print(line)
                     elif(len(line)>=14):
                         x=line.split(' ')
                         fp.write(x[0]+"\n")
+                        print(line)
                if number == 5: #razon social
+                    print(number, line)
+                    print(len(line))
                     if (len(line)!=35):
                         fp.write(line)
+                        print(line)
                if number ==  6:
-                   if(len(line)!=38):
+                   print(number, line)
+                   print(len(line))
+                   if(len(line)!=38):#razon social con cadena encimado de 'Nombre'
                         x=line.split(' ')
                         arr=[]
                         for i in x:
@@ -36,38 +46,74 @@ def borrar(nombreAr):
                             elif (i=='Nombre,'):
                                 break
                         fp.write(" ".join(arr)+"\n")
+                        print(" ".join(arr)+"\n")
                if number == 45: #codigo postal
+                     print(number, line)
+                     print(len(line))
                      if (len(line)!=1):
                          if (len(line) == 21):
                              x=line.split(':')
                              fp.write(x[1])
+                             print(line)
                          else:
                              fp.write(line)
-               if number == 46: #codigo postal y tipo de vialidad 
+                             print(line)
+               if number == 46: #codigo postal y nombre de vialidad 
+                    print(number, line)
+                    print(len(line))
                     if (len(line)!=1):
                       if (len(line) == 21): #division del codigo postal 
                              x=line.split(':')
-                             print(x)
                              fp.write(x[1])
-                      else:
-                             fp.write(line)
-               if number == 47:#codigo postal y tipo de vialidad
+                             print(x[1])
+                      elif(len(line) >=22): #numero interior
+                              x=line.partition('NÃºmero Interior:') 
+                              x1=list(x)
+                              print(x1)
+                              fp.write(x1[0]+"\n") #guarda nombre de vialidad 
+                              fp.write(x1[1]+" "+x1[2])#guarda numero interior
+                              print(x1[0])
+                              print(x1[1]+" "+x1[2])
+                    else:
+                            x=line.partition
+               if number == 47:#codigo postal y nombre de vialidad
+                    print(number, line)
+                    print(len(line))
                     if (len(line)!=1):
-                      if (len(line) == 21):
+                      if (len(line) == 21):#division del codigo postal
                              x=line.split(':')
-                             print(x)
                              fp.write(x[1])
-                      else:
-                             fp.write(line)
-               if number == 48:#tipo de vialidad
+                             print(x[1])
+                      elif(len(line) >=22): #numero interior
+                              x=line.partition('NÃºmero Interior:') 
+                              x1=list(x)
+                              print(x1)
+                              fp.write(x1[0]+"\n") #guarda nombre de vialidad 
+                              fp.write(x1[1]+" "+x1[2])#guarda numero interior
+                              print(x1[0])
+                              print(x1[1]+" "+x1[2])
+                    else:
+                            x=line.partition
+               if number == 48:#nombre de vialidad
+                    print(number,line)
+                    print(len(line))
                     if (len(line)!=1):
-                      if (len(line) == 21):
-                             x=line.split(':')
-                             print(x)
-                             fp.write(x[1])
-                      else:
-                             fp.write(line)
+                        if(len(line) >=22): #numero interior
+                              x=line.partition('NÃºmero Interior:') 
+                              x1=list(x)
+                              print(x1)
+                              fp.write(x1[0]+"\n") #guarda nombre de vialidad 
+                              fp.write(x1[1]+" "+x1[2])#guarda numero interior
+                              print(x1[0])
+                              print(x1[1]+" "+x1[2])
+                    else:
+                            x=line.partition
+               if number == 49:#
+                    print(number,line)
+                    if (1==1):
+                        fp.write(line)
                if number == 50:#
+                    print(number, line)
                     if (1==1):
                         fp.write(line)
                if number == 51:
@@ -97,10 +143,11 @@ def borrar(nombreAr):
                #fp.write(line)
     os.system("Pause")
 
-def inicio():
+
+def inicio():#funcion en donde se introduce la ruta del archivo y el nombre del archivo 
         nombreAr = input("introduzca el nombre del archivo que se encuentra en la direccion: C:/Users/mmartinez/Documents/CSF/")
         archivo =  os.path.join("C:/Users/mmartinez/Documents/CSF/" + nombreAr +".pdf")
         conviertePDFaTXT(archivo,nombreAr)
         borrar(nombreAr)
 
-inicio()
+inicio()#inici del programa
